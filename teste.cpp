8b0025d843dee9ea5bf7d5d2442dfb5509b3462a
Guilherme Daros <gh5777.gh@gmail.com>
2021-04-08 21:41:31 -0300

    Add files via upload

---
+++ teste.cpp
@@ -0,0 +1,10 @@
+#include "ClockCalendar.cpp"
+#include <iostream>
+
+ClockCalendar cc;
+
+int main() {
+    std::cout << cc.getDate() << std::endl;
+    std::cout << cc.getTime() << std::endl;
+
+}
