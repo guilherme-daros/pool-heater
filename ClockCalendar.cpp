96234b30a0af80880b23999122e62f6ee6d2cef5
Guilherme Daros <gh5777.gh@gmail.com>
2021-04-09 00:08:11 -0300

    final

--- ClockCalendar.cpp
+++ ClockCalendar.cpp
@@ -27,16 +27,16 @@ ClockCalendar::~ClockCalendar() {
 std::string ClockCalendar::getTime() {
     std::time_t t = std::time(0);   // get time now
     std::tm* now = std::localtime(&t);
-    char buffer1[10];
-    sprintf(buffer1, "%i:%i:%i", now->tm_mday, now->tm_mon + 1, now->tm_year + 1900);
-    return (buffer1);
+    std::ostringstream buffer;
+    buffer << now->tm_hour << ':' << now->tm_min << ':' << now->tm_sec;
+    return (buffer.str());
 }
 
 std::string ClockCalendar::getDate() {
     std::time_t t = std::time(0);   // get time now
     std::tm* now = std::localtime(&t);
-    char buffer2[10];
-    sprintf(buffer2, "%i:%i:%i", now->tm_mday, now->tm_mon + 1, now->tm_year + 1900);
-    return (buffer2);
+    std::ostringstream buffer;
+    buffer << now->tm_mday << '/' << now->tm_mon + 1 << '/' << now->tm_year + 1900;
+    return (buffer.str());
 }
 
