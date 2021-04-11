fef043c5d136e8e31e0dd03df80001df9bcaafb4
Guilherme Daros <gh5777.gh@gmail.com>
2021-04-11 19:17:20 -0300

    Serial Menu Implemented

--- Host/run.cpp
+++ Host/run.cpp
@@ -4,18 +4,77 @@
       o  aquecedor alimentado, em um determinado intervalo de datas;
   •listar o período do dia com mais utilização do sistema de controle.
 */
-#include <string>
-#include <ctime>
+
 #include <iostream>
+#include <ctime>
+#include <string>
 #include "SerialWindows.cpp"
+#include "List.cpp"
+
 using namespace std;
 
-SerialWindows serial;
+void listAll();
+void uptimeBetween();
+void biggestPeriodBetween();
+
+SerialWindows serial("COM4", 9600);
+ostringstream buffer;
+List logger;
+unsigned char data;
 int sel;
 
 
+
 int main() {
-  while (sel != 0) {
-    cout << "" << endl;
+  buffer << " ";
+  while(1){
+    serial.flush();
+    while(buffer.str() != ""){
+      buffer.clear();
+      buffer.str("");
+      while(data != '\n'){
+        serial.read(data,1);
+        buffer << data;
+      }
+      logger.insertBeforeFirst(buffer.str());
+    }
+
+    cout << "==================PoolHeaterViewer==================" << endl;
+    cout << "[0] - Exit" << endl;
+    cout << "[1] - List All Events" << endl;
+    cout << "[2] - Show events in a period of time" << endl;
+    cout << "[3] - Show bigger on-period in a period of time" << endl;
+    cout << "Option: ";
+    cin >> sel;
+    string time1, time2;
+    switch (sel)
+    {
+    case 0:
+      system("PAUSE");
+      return 0;
+      break;
+    case 1:
+      listAll();
+    case 2:
+      cout << "Begin time: ";
+      cin >> time1;
+      cout << "End time: ";
+      cin >> time2;
+      cout << "Logs betwenn " << time1 << " and " << time2 << endl;
+      uptimeBetween();
+      time1 = time2 = "";
+    case 3:
+      cout << "Begin time: ";
+      cin >> time1;
+      cout << "End time: ";
+      cin >> time2;
+      cout << "Logs betwenn " << time1 << " and " << time2 << endl;
+      biggestPeriodBetween();
+      time1 = time2 = "";
+    default:
+      break;
+    }
   }
 }
+
+ 
